747dfc9f9e7d1e88713ea425ad50963fc35ea4c8
marwenkhlifi15 <marwen.khlifi15@outlook.com>
2026-06-22 19:43:36 +0100

    Initial commit

---
+++ src/components/Layout/UserLayout.jsx
@@ -0,0 +1,14 @@
+import Header from '../common/Header';
+
+const UserLayout = () => {
+  return (
+    <>
+      {/* Header */}
+      <Header />
+      {/* Main content */}
+      {/* Footer */}
+    </>
+  );
+};
+
+export default UserLayout;
