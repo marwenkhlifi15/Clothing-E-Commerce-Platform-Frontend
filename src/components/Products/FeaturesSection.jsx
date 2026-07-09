eee39cf7f458698272fff72c546500725cd4618e
marwenkhlifi15 <marwen.khlifi15@outlook.com>
2026-07-09 16:25:05 +0100

    Editing Features section

---
+++ src/components/Products/FeaturesSection.jsx
@@ -0,0 +1,11 @@
+const FeaturesSection = () => {
+  return (
+    <section className="py-16 px-4 bg-white">
+      <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-8 text-center">
+        {/* Feature 1 */}
+      </div>
+    </section>
+  );
+};
+
+export default FeaturesSection;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import Hero from '../components/Layout/Hero';
 import FeaturedCollection from '../components/Products/FeaturedCollection';
+import FeaturesSection from '../components/Products/FeaturesSection';
 import GenderCollectionSection from '../components/Products/GenderCollectionSection';
 import NewArrivals from '../components/Products/NewArrivals';
 import ProductDetails from '../components/Products/ProductDetails';
@@ -72,6 +73,7 @@ const Home = () => {
       </div>
       <ProductGrid products={placeHolderProducts} />
       <FeaturedCollection />
+      <FeaturesSection />
     </div>
   );
 };
