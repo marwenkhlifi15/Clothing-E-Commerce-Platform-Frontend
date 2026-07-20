6d79982560ad1a307f4b18f0561507bc13fcc05f
marwenkhlifi15 <marwen.khlifi15@outlook.com>
2026-07-20 19:26:24 +0100

    Complete collection page and filter

--- src/pages/CollectionPage.jsx
+++ src/pages/CollectionPage.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useRef, useState } from 'react';
 import { FaFilter } from 'react-icons/fa';
 import FilterSidebar from './FilterSidebar';
+import SortOptions from '../components/Products/SortOptions';
+import ProductGrid from '../components/Products/ProductGrid';
 
 const CollectionPage = () => {
   const [products, setProducts] = useState([]);
   const sidebarRef = useRef(null);
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-
   const toggleSidebar = () => {
     setIsSidebarOpen(!isSidebarOpen);
   };
@@ -92,9 +93,16 @@ const CollectionPage = () => {
       {/* Filter Sidebar */}
       <div
         ref={sidebarRef}
-        className={`${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'}`}
+        className={`${isSidebarOpen ? 'translate-x-0' : '-translate-x-full'} fixed inset-y-0 z-50 left-0 w-64 bg-white overflow-y-auto transition-transform duration-300 lg:static lg:translate-x-0`}
       >
         <FilterSidebar />
+      </div>
+      <div className="grow p-4">
+        <h2 className="text-2xl uppercase mb-4">All Collections</h2>
+        {/* Sort Options */}
+        <SortOptions />
+        {/* ProductGrid */}
+        <ProductGrid products={products} />
       </div>
     </div>
   );
