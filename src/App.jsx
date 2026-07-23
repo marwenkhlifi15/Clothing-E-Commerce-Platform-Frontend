4e5288786917696e29ec46183dd8bc54352c9245
marwenkhlifi15 <marwen.khlifi15@outlook.com>
2026-07-23 15:07:41 +0100

    creaing Admin Home Page

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,8 @@ import Checkout from './components/Cart/Checkout';
 import OrderConfirmationPage from './pages/OrderConfirmationPage';
 import OrderDetailsPage from './pages/OrderDetailsPage';
 import MyOrdersPage from './pages/MyOrdersPage';
+import AdminLayout from './components/Admin/AdminLayout';
+import AdminHomePage from './pages/AdminHomePage';
 
 const App = () => {
   return (
@@ -32,7 +34,9 @@ const App = () => {
           <Route path="order/:id" element={<OrderDetailsPage />}></Route>
           <Route path="my-orders" element={<MyOrdersPage />}></Route>
         </Route>
-        <Route>{/* Admin Layout */}</Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route index element={<AdminHomePage />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
